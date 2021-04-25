592f7819d6dfab08fe46812494701ebc20a11b26
GuilhermePontes1 <guilhermepontes58@hotmail.com>
2021-04-25 19:16:08 -0300

    Adicionando a relação com o banco de dados H2 e fazendo a relação entre as três tabelas com ManyToMany além de incluir a classe produtos para melhor organização da loja

--- src/main/java/com/guilherme/cursoMC/CursoMcApplication.java
+++ src/main/java/com/guilherme/cursoMC/CursoMcApplication.java
@@ -1,13 +1,52 @@
 package com.guilherme.cursoMC;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import com.guilherme.cursoMC.domain.Categoria;
+import com.guilherme.cursoMC.domain.Produto;
+import com.guilherme.cursoMC.repositories.CategoriaRepository;
+import com.guilherme.cursoMC.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner {
+
+	@Autowired //Instancia automáticamente assim que o código é executado
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// Aqui realizamos as associações das categorias com os produtos de mão dupla, um reconhecendo o outro
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 100.00);
+		Produto p3 = new Produto(null, "Mouse", 800.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat1.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+	
 	}
 
 }
