67595be4668a34c95bd95f5ebc0a9faaf5693ad9
GuilhermePontes1 <guilhermepontes58@hotmail.com>
2021-04-27 01:05:26 -0300

    .

--- src/test/java/com/guilherme/SpringBoot_Marketplace/CursoMcApplicationTests.java
+++ src/test/java/com/guilherme/SpringBoot_Marketplace/CursoMcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CursoMcApplicationTests {
+class SpringBoot_MarketplaceApplicationTests {
 
 	@Test
 	void contextLoads() {
