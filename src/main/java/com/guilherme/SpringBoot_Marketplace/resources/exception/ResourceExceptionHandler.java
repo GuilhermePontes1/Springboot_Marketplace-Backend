6a654b30e8258285f283d268c42308350ba8278d
GuilhermePontes1 <guilhermepontes58@hotmail.com>
2021-08-02 23:54:27 -0300

    Padronizando formato das exceções :rocket:

--- src/main/java/com/guilherme/SpringBoot_Marketplace/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/guilherme/SpringBoot_Marketplace/resources/exception/ResourceExceptionHandler.java
@@ -23,7 +23,7 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(ObjectNotFoundException.class) // para indicar que é uma exceção desse tipo de exceção
     public ResponseEntity<StandardError> objectNotFoud(ObjectNotFoundException e, HttpServletRequest request) {
         // em cima é padrão da anotação ControllerAdvice
-        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "NÃO ENCONTRADO", e.getMessage(), request.getRequestURI());
         // em cima estamos instanciando o erro que irá retornar o notFound no caso 404,
         // o id que procura além do
         // pacote e o tempo de execução
@@ -34,38 +34,37 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandardError> DataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err); // aqui digitamos os tipos de erros que queremos que apareçam
-        // para o usuário
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "INTEGRIDADE DE DADOS", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err); // aqui digitamos os tipos de erros que queremos que apareçam para o usuário
     }
 
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
-
-        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+                                                                                        //UNPROCESSABLE_ENTITY = erro 422 descrito como erro de processamento de formulário
+        ValidationError err = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "ERRO DE VALIDAÇÃO", e.getMessage(), request.getRequestURI());
 
         for (FieldError x : e.getBindingResult().getFieldErrors()) {
             err.addError(x.getField(), x.getDefaultMessage()); // procura os erros dentro do método "MethodArgumentNotValidException" adiciona eles a lista que irá ser mostrada
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
     }
 
     @ExceptionHandler(AuthorizationException.class) // para indicar que é uma exceção desse tipo de exceção
     public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request) {
         // em cima é padrão da anotação ControllerAdvice
-        StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(), "ACESSO NEGADO", e.getMessage(), request.getRequestURI());
         // em cima estamos instanciando o erro que irá retornar o notFound no caso FORBIDDEN OU acesso negado,
         // o id que procura além do
         // pacote e o tempo de execução
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
     }
 
     @ExceptionHandler(FileException.class) // para indicar que é uma exceção desse tipo de exceção
     public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request) {
         // Erro personalizado na execução de envio de arquivos enviados para o amazon AWS
-        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "ERRRO DE ARQUIVO ", e.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
@@ -75,7 +74,7 @@ public class ResourceExceptionHandler {
 
 
         HttpStatus code = HttpStatus.valueOf(e.getErrorCode()); // pega o códigoo http que veio da exceção e transforma para objeto http do tipo status para mostrar o erro ao usuárrio
-        StandardError err = new StandardError(code.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(),code.value(), "ERRO AMAZON SERVICE", e.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(code).body(err);
 
@@ -84,14 +83,14 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(AmazonClientException.class)
     public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "ERRRO AMAZON CLIENT", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(AmazonS3Exception.class)
     public ResponseEntity<StandardError> amazonS3(AmazonS3Exception e, HttpServletRequest request) {
 
-        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "ERRRO AMAZON S3", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
    // Classes para gerar erros personalizados em geral
